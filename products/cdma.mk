5afdc6732aa57c3c64567a91d5f619eae8c530c9
DHOMD <DHO@dho.im>
2013-09-11 16:09:48 -0400

    Let US only spectrums speak English
    GSM can have MegaBytes of bloat.... I mean extended languauge support
    
    Change-Id: I3a4f15672bbbdf4acb92235d29536d20ae10463c

--- products/cdma.mk
+++ products/cdma.mk
@@ -4,3 +4,6 @@ PRODUCT_COPY_FILES += \
 
 PRODUCT_PACKAGE_OVERLAYS += \
     vendor/vanir/overlay/cdma_phone
+
+#... and then make the AOSP device configuration for Verizon Phones get on its knees and sing the national anthem into our "microphones"
+-PRODUCT_LOCALES := en_US 
