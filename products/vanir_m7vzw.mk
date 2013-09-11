ea6eb63c0744c1b5b5a1a20d0b62817fd42c75ee
Trevin Gandhi <sonicxml@gmail.com>
2013-09-11 18:28:50 -0400

    Add NFC
    Change-Id: Ifdab9875a83daab1683056111e4c91d922b13294

--- products/vanir_m7vzw.mk
+++ products/vanir_m7vzw.mk
@@ -5,12 +5,14 @@ PRODUCT_PROPERTY_OVERRIDES += \
     drm.service.enabled=true \
     ro.goo.rom=vanir-m7vzw
 
-# Inherit AOSP device configuration for toro.
+# Inherit AOSP device configuration for m7.
 $(call inherit-product, device/htc/m7vzw/full_m7vzw.mk)
 
 $(call inherit-product, vendor/vanir/products/vanir_m7.mk)
 
 $(call inherit-product, vendor/vanir/products/cdma.mk)
+
+$(call inherit-product, vendor/vanir/config/nfc_enhanced.mk)
 
 # Setup device specific product configuration.
 PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=m7wlv BUILD_ID=JZO54K BUILD_FINGERPRINT="VERIZON/HTCOneVZW/m7wlv:4.2.2/JDQ39/221778.8:user/release-k" PRIVATE_BUILD_DESC="1.10.605.8 CL221778 release-keys"
