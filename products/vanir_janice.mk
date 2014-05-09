d019a237e1f2b8f03d014a6675596932d5ebbbff
28vicky <28vicky@users.noreply.github.com>
2014-05-09 19:31:25 +0530

    Create vanir_janice.mk

---
+++ products/vanir_janice.mk
@@ -0,0 +1,27 @@
+# Inherit AOSP device configuration for janice
+$(call inherit-product, device/samsung/janice/full_janice.mk)
+ 
+# Inherit common phone stuff
+$(call inherit-product, vendor/vanir/products/common.mk)
+
+# Inherit common vanir files.
+$(call inherit-product, vendor/vanir/products/gsm.mk)
+
+# Release name
+PRODUCT_RELEASE_NAME := GT-I9070
+
+# Overlay
+#PRODUCT_PACKAGE_OVERLAYS += vendor/aicp/overlay/janice
+ 
+# Setup device specific product configuration
+PRODUCT_DEVICE := janice
+PRODUCT_NAME := vanir_janice
+PRODUCT_BRAND := samsung
+PRODUCT_MODEL := GT-I9070
+PRODUCT_MANUFACTURER := samsung
+
+# Boot Animation
+PRODUCT_COPY_FILES += \
+    vendor/vanir/proprietary/boot_animations/480x800.zip:system/media/bootanimation.zip
+
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=GT-I9070 TARGET_DEVICE=janice
