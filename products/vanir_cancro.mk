f8c0a02204a624501c2a131239ed7164e83811c8
DHOMD <DHO@dho.im>
2015-12-21 20:16:18 -0500

    Fixup cancro to work w/ cm-13.0
    Change-Id: I4fe9806a61d0d94450f0c61e593967405efad782

--- products/vanir_cancro.mk
+++ products/vanir_cancro.mk
@@ -9,10 +9,8 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.goo.rom=vanir-cancro
 
 # Inherit device configuration
-$(call inherit-product, device/xiaomi/cancro/full_cancro.mk)
+$(call inherit-product, device/xiaomi/cancro/cancro.mk)
 
-# Enhanced NFC
-$(call inherit-product, vendor/vanir/config/nfc_enhanced.mk)
 
 # Inherit some common Vanir stuff 
 $(call inherit-product, vendor/vanir/products/common_phones.mk)
@@ -22,17 +20,15 @@ PRODUCT_NAME := vanir_cancro
 PRODUCT_DEVICE := cancro
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MANUFACTURER := Xiaomi
-PRODUCT_MODEL := MI3W
+PRODUCT_MODEL := MI Cancro
+
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
-TARGET_VENDOR := xiaomi
-TARGET_VENDOR_PRODUCT_NAME := cancro
-TARGET_VENDOR_DEVICE_NAME := MI3W
+TARGET_CONTINUOUS_SPLASH_ENABLED := true
 
-# Unofficial build id
-TARGET_UNOFFICIAL_BUILD_ID := audahadi
-
-PRODUCT_BUILD_PROP_OVERRIDES += TARGET_DEVICE=cancro PRODUCT_NAME=cancro
-
-## Use the latest approved GMS identifiers unless running a signed build
-PRODUCT_BUILD_PROP_OVERRIDES += BUILD_FINGERPRINT=Xiaomi/cancro/cancro:6.0/MDB08M/5.11.6:userdebug/test-keys PRIVATE_BUILD_DESC="cancro-userdebug 6.0 MDB08M 5.11.6 test-keys"
+# Device prop
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    TARGET_DEVICE="cancro" \
+    PRODUCT_NAME="cancro" \
+    BUILD_FINGERPRINT="Xiaomi/cancro/cancro:5.1.1/LMY48M/5.9.17:userdebug/test-keys" \
+    PRIVATE_BUILD_DESC="cancro-userdebug 5.1.1 LMY48M 5.9.17 test-keys"
