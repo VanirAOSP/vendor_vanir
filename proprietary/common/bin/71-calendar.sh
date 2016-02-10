af2fea957d1a68af432b21eddedd27988fe307b1
nuclearmistake <nuclearmistake@gmail.com>
2016-02-09 21:37:46 -0500

    backuptool: remove AOSP calendar from system if Google's calendar is found to be installed (from the play store)
    Change-Id: I548c12c685c3701cc1de5162ab42c0ae4697e69e

---
+++ proprietary/common/bin/71-calendar.sh
@@ -0,0 +1,24 @@
+#!/sbin/sh
+#
+# /system/addon.d/71-calendar.sh
+# Automagically fix duplicate calendars by removing AOSP calendar from /system when Google Calendar is installed from the play store
+#
+# BIG props to Chainfire for the stdout redirect hautness
+# BIG props nuclearmistake for TWRP-izing it
+export OUTFD=$(ps | grep -v "grep" | grep -o -E "update_binary(.*)" | cut -d " " -f 3);
+[ ! $OUTFD ] && export OUTFD=$(ps | grep -v "grep" | grep -o -E "/tmp/updater(.*)" | cut -d " " -f 3);
+ui_print() { if [ $OUTFD ]; then echo "ui_print $*" 1>&$OUTFD; fi; return 0; }
+
+. /tmp/backuptool.functions
+
+case "$1" in
+  post-restore)
+    if ls /data/app | grep -q com.google.android.calendar-; then
+      ui_print "Auto-removing AOSP calendar from /system because you have installed Google calendar from the Play store"
+      rm -Rf $S/app/Calendar
+    fi
+  ;;
+  *)
+    # no-op
+  ;;
+esac
