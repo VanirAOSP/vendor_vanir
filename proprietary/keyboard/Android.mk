c357982deb712d8c267d2bfa0a87adf5883ed9fe
DHOMD <DHO@dho.im>
2016-12-19 21:32:51 -0500

    Stash this possible latinIME commit
    Change-Id: I83021d39468d1af7d5933759d5f2614440a54912
    Signed-off-by: DHOMD <DHO@dho.im>

--- proprietary/keyboard/Android.mk
+++ proprietary/keyboard/Android.mk
@@ -1,6 +1,28 @@
 LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
+LOCAL_MODULE := Latinime
+LOCAL_MODULE_SUFFIX :=.so
+LOCAL_MODULE_CLASS := SHARED_LIBRARIES
+LOCAL_MODULE_TAGS := optional
+LOCAL_PRELINK_MODULE := false
+ifdef TARGET_2ND_ARCH
+LOCAL_MULTILIB := both
+LOCAL_MODULE_PATH_64 := system/lib64
+LOCAL_MODULE_PATH := lib64/libjni_latinimegoogle.so
+LOCAL_MODULE_PATH_32 := system/lib
+LOCAL_MODULE_PATH := lib/libjni_latinimegoogle.so
+else
+LOCAL_MODULE_PATH := system/lib
+LOCAL_MODULE_PATH := lib/libjni_latinimegoogle.so
+endif
+include $(BUILD_PREBUILT)
+
+
+##### Now Let's Symlink
+ifeq ($(TARGET_ARCH),arm)
+
+include $(CLEAR_VARS)
 
 LOCAL_MODULE := Latinimegoogle
 LOCAL_MODULE_OWNER := google
@@ -11,10 +33,11 @@ LOCAL_POST_INSTALL_CMD := \
 
 include $(BUILD_PHONY_PACKAGE)
 
+else
 
 include $(CLEAR_VARS)
 
-LOCAL_MODULE := Latinimegoogle64
+LOCAL_MODULE := Latinimegoogle
 LOCAL_MODULE_OWNER := google
 LOCAL_MODULE_TAGS := optional
 
@@ -22,3 +45,5 @@ LOCAL_POST_INSTALL_CMD := \
     ln -sf /system/lib64/libjni_latinime.so $(PRODUCT_OUT)/system/lib64/libjni_latinimegoogle.so
 
 include $(BUILD_PHONY_PACKAGE)
+
+endif
