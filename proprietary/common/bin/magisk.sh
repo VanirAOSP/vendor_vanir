3010114233aa7ee3dcf061a11666b06dcb29dda9
nuclearmistake <nuclearmistake@gmail.com>
2017-04-07 08:53:25 -0400

    Fix magisk auto-flash
    Change-Id: I81803d50b1e401a296907b66127e79f5aaeb9139

--- proprietary/common/bin/magisk.sh
+++ proprietary/common/bin/magisk.sh
@@ -8,10 +8,10 @@ export OUTFD=$(ps | grep -v "grep" | grep -o -E "update_binary(.*)" | cut -d " "
 ui_print() { if [ $OUTFD ]; then echo "ui_print $*" 1>&$OUTFD; else echo $*; fi; return 0; }
 
 ui_print "Unpacking magisk installer script..."
-busybox unzip /tmp/install/magisk.zip META-INF/com/google/android/ -d /tmp/magisk  2>&1 | while read output; do ui_print $output; done
+busybox unzip /tmp/install/magisk.zip -d /tmp/magisk  2>&1 | while read output; do ui_print $output; done
 
 ui_print "Installing magisk..."
-/sbin/sh /tmp/magisk/update-binary dummy 1 /tmp/magisk/magisk.zip 2>&1 | while read output; do ui_print $output; done
+/sbin/sh /tmp/magisk/META-INF/com/google/android/update-binary dummy $OUTFD /tmp/magisk/magisk.zip 2>&1 | while read output; do ui_print $output; done
 
 ui_print ""
 ui_print "Have fun android paying and pokemon going, suckas"
