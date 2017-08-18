1383758442eec9a50f97bbef54dce21f14c19188
Luca Stefani <luca020400@lineageos.org>
2017-08-18 10:23:30 +0200

    vendor: Kill sepolicy
    * Will be re-written in device/lineage
    
    Change-Id: I755d129efbc69b712a20833b7b51187bfd66e844

--- sepolicy/recovery-twrp/sepolicy.mk
+++
@@ -1,8 +0,0 @@
-#
-# This policy configuration will be used by all products that
-# don't specify a RECOVERY_VARIANT
-#
-
-BOARD_SEPOLICY_DIRS += \
-    bootable/recovery-twrp/sepolicy \
-    vendor/vanir/sepolicy/recovery-twrp
--- sepolicy/recovery/sepolicy.mk
+++
@@ -1,7 +0,0 @@
-#
-# This policy configuration will be used by all products that
-# don't specify a RECOVERY_VARIANT
-#
-
-BOARD_SEPOLICY_DIRS += \
-    vendor/vanir/sepolicy/recovery
